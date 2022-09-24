565ab1aa8f46eb13b76cc8d38903d173b45f5e5c
nielsdelestinne <nielsdelestinne@gmail.com>
2022-09-24 16:19:00 +0200

    Removed @ng-bootstrap/ng-bootstrap (not going to use it after all)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {HttpClientModule} from "@angular/common/http";
 import {NgxEchartsModule} from "ngx-echarts";
 import {PowerInjectionComponent} from './power-injection/power-injection.component';
@@ -32,7 +31,6 @@ import { TotalPowerComponent } from './total-power/total-power.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule,
     HttpClientModule,
     NgxEchartsModule.forRoot({
       echarts: () => import('echarts')
